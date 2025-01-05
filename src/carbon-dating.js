4bbdcb161f25f08bd9670d4766f7551c23c1c7df
dianakhnizova <khnizovad@gmail.com>
2025-01-05 13:46:41 +0600

    feat: add task - hanoi-tower

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+
+  if (typeof sampleActivity !== 'string' || isNaN(sampleActivity) || +sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  const A = parseFloat(sampleActivity);
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / A) / (0.693 / HALF_LIFE_PERIOD));
+
+  return age;
 }
 
 module.exports = {
