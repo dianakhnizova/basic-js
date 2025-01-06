fa4ad95976e77edd432af6e44c3d3cdcd190191b
dianakhnizova <khnizovad@gmail.com>
2025-01-06 13:53:32 +0600

    feat: add task - extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+  const addition = options.addition === undefined ? '' : String(options.addition);
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+  
+  let additionStr = '';
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    additionStr += addition;
+    if (i < additionRepeatTimes - 1) {
+      additionStr += additionSeparator;
+    }
+  }
+
+  let resStr = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    resStr += str + additionStr;
+    if (i < repeatTimes - 1) {
+      resStr += separator;
+    }
+  }
+  return resStr;
 }
 
 module.exports = {
