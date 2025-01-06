14169a5835c04ab6700f3cef0adebf808ab530c9
dianakhnizova <khnizovad@gmail.com>
2025-01-06 16:30:24 +0600

    feat: add task - dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const static = {};
+  for (let i = 0; i < domains.length; i += 1) {
+     const domainParts = domains[i].split('.').reverse();
+     let domainsStr = '';
+     for (let j = 0; j < domainParts.length; j += 1) {
+      domainsStr = `${domainsStr}.${domainParts[j]}`;
+      static[domainsStr] = (static[domainsStr] || 0) + 1;
+     }
+  }
+  return static;
 }
 
 module.exports = {
